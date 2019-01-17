06e1bd17b76486a827f8f0f48d19c2afc7dc7375
unknown <fdininno92@gmail.com>
2019-01-17 11:47:24 +0100

    draw tree completed

--- test.py
+++ test.py
@@ -2,11 +2,6 @@ import turtle as tu
 import random 
 from collections import deque
 
-stack = deque()
-stack_heading = deque()
-c = 100
-#recursion
-
 def get_turtle_state(turtle):
     """ Return turtle's current heading and position. """
     return turtle.heading(), turtle.position()
@@ -16,22 +11,19 @@ def restore_turtle_state(turtle, state):
     turtle.setheading(state[0])
     turtle.setposition(state[1][0], state[1][1])
 
-def draw2(l,iterations):
-    l = 3*l/4
-    #turtle.screen.tracer(0, 0)
-    for i in range(2**iterations):
-        turtle.penup()
-        restore_turtle_state(turtle, stack.popleft())
-        turtle.pendown()
+def draw_depth_infinite(l,iterations=0):
+        """ Draw infinite fractal """
+        l = 3*l/4
+        #turtle.screen.tracer(0, 0)
+        for i in range(2**iterations):
+                turtle.penup()
+                restore_turtle_state(turtle, stack.popleft())
+                turtle.pendown()
+                draw_leaf(l)
+                turtle.backward(l)
+        draw_depth_infinite(l, iterations+1)
 
-        draw_leaf(l)
-        turtle.backward(l)
-        #turtle.screen.update()
-        #print(stack)
-    
-    draw2(l, iterations+1)
-
-def draw3(iterations, branches = 2, alpha = 60):
+def draw_breadth_first(iterations, branches = 2, alpha = 60):
     l=100
     for j in range(iterations): 
         print(j)
@@ -46,32 +38,35 @@ def draw3(iterations, branches = 2, alpha = 60):
             draw_leaf(l, branches, alpha)
 
         turtle.screen.update()
-        l = 3*l/4
+        l = l * ((branches-1)/branches)
 
 def draw_leaf(l, branches = 2, alpha = 60 ):
-    turtle.left(alpha/2)
-    increment = alpha * 1/(branches-1)
-    turtle.forward(l)
-    stack.append(get_turtle_state(turtle))
-    turtle.backward(l)
-    for b in range(1,branches):
-        turtle.right(increment)   
+        """draw leaf with a total angle alpha and branches"""
+        turtle.left(alpha/2)
+        increment = alpha * 1/(branches-1)
         turtle.forward(l)
         stack.append(get_turtle_state(turtle))
         turtle.backward(l)
+        for b in range(1,branches):
+                turtle.right(increment)   
+                turtle.forward(l)
+                stack.append(get_turtle_state(turtle))
+                turtle.backward(l)
 
 
+def init(turtle):
+        """init stack and turtle"""
+        turtle.left(90)
+        stack.append(get_turtle_state(turtle))
 
-def init_stack():
-    turtle = tu.Turtle()
-    turtle.left(90)
-    stack.append(get_turtle_state(turtle))
-    return turtle
+def main():
+        init(turtle)
+        draw_breadth_first(iterations = 10, branches= 3, alpha = 60)
+        #save result as post script file
+        ts = turtle.getscreen()
+        ts.getcanvas().postscript(file="post_script_result.ps")
+        input("Press Enter to continue...")
 
-turtle = init_stack()
-
-draw3(6, 3, 245)
-ts = turtle.getscreen()
-ts.getcanvas().postscript(file="C:\\Users\\Administrator\\Desktop\\duck.ps")
-
-input("Press Enter to continue...")
+turtle = tu.Turtle()
+stack = deque()
+main()
