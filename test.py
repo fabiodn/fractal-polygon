ff6b3621659f24f5c8963b4b412887edc425f083
unknown <fdininno92@gmail.com>
2019-01-18 10:10:23 +0100

    tresso formula test

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import turtle as tu
 import random 
 from collections import deque
+import math
 
 def get_turtle_state(turtle):
     """ Return turtle's current heading and position. """
@@ -53,11 +54,106 @@ def draw_leaf(l, branches = 2, alpha = 60 ):
                 stack.append(get_turtle_state(turtle))
                 turtle.backward(l)
 
+def draw_polygon(sides , l  , position, save = False):
+        alpha = 180 / sides
+        turtle.penup()
+        restore_turtle_state(turtle, position)
+        turtle.setheading(0)
+        turtle.pendown()
+
+        turtle.forward(l)
+        if(save):
+                stack.append(get_turtle_state(turtle))
+        for s in range(1,sides-1):
+                turtle.left(alpha*2)
+                turtle.forward(l)
+                if(save):
+                        stack.append(get_turtle_state(turtle))
+        turtle.left(alpha*2)
+        turtle.forward(l)
+
+
+
+
+def draw_inverted_polygon(sides = 3, l = 100):
+        alpha = 180 / sides
+        turtle.penup()
+        restore_turtle_state(turtle, stack.popleft())
+        turtle.pendown()
+
+        turtle.left(alpha)
+        turtle.forward(l)
+        stack.append(get_turtle_state(turtle))
+        for s in range(1,sides):
+                turtle.left(alpha*2)
+                turtle.forward(l)
+                stack.append(get_turtle_state(turtle))
+
+def draw_fractal_polygon(sides, l ):
+        iterations = 5
+        
+        for i in range(iterations):
+                turtle.color("#00{:02x}ff".format(int(255*i/iterations)))
+                turtle.pensize(int(10*(1-i/iterations)))
+                turtle.screen.tracer(0, 0)
+                n = len(stack)
+                for s in range(n*sides):
+                        if s < n:
+                                draw_polygon(sides, l/2, stack[s], True)
+                        else:
+                                draw_polygon(sides, l/2, stack[s], False)
+                turtle.screen.update()
+                l = l / 2
 
 def init(turtle):
         """init stack and turtle"""
         turtle.left(90)
         stack.append(get_turtle_state(turtle))
+
+def draw_relative_polygon(sides, l,position, heading):
+        alpha = 180 / sides
+        turtle.penup()
+        restore_turtle_state(turtle, position)
+        turtle.setheading(turtle.heading() + heading)
+        turtle.pendown()
+
+        stack.append(get_turtle_state(turtle))
+        turtle.forward(l)
+        stack.append(get_turtle_state(turtle))
+        for s in range(1,sides-1):
+                turtle.left(alpha*2)
+                turtle.forward(l)
+                stack.append(get_turtle_state(turtle))
+        turtle.left(alpha*2)
+        turtle.forward(l)
+        #stack.append(get_turtle_state(turtle))
+
+def draw_relative_polygon_init(sides, l):
+        alpha = 180 / sides
+
+        turtle.forward(l)
+        stack.append(get_turtle_state(turtle))
+        for s in range(1,sides):
+                turtle.left(alpha*2)
+                turtle.forward(l)
+                stack.append(get_turtle_state(turtle))
+
+
+def draw_fractal_relative_polygon(sides, l, iteration= 0):
+        draw_relative_polygon_init(sides, l)
+        l = l/2
+        alpha = 180 / sides
+        
+        for j in range(5):
+                n = len(stack)
+                for i in range(n):        
+                        alpha = (alpha*2*i)%360
+                        print("i:{} iteration:{} alpha:{}".format(i, iteration, alpha))
+                        position = stack.popleft()
+
+                        draw_relative_polygon(sides, l,position,alpha)
+                iteration = iteration + 1
+                l= l/2
 
 def main():
         init(turtle)
@@ -65,8 +161,42 @@ def main():
         #save result as post script file
         ts = turtle.getscreen()
         ts.getcanvas().postscript(file="post_script_result.ps")
-        input("Press Enter to continue...")
 
+def tresso_formula(l, n):
+        b = 360/n * int((n-1)/4)
+        b = math.radians(b)
+        l = l/2 * (1+ (math.sin(b/2)*math.cos(b)) / (math.tan(math.pi/n)*math.cos(math.pi/n)))**-1
+        return l
+
+def test_tresso_formula(sides):
+        l=100
+        iterations = 5
+        bho = 5
+
+        alpha = 360 / sides
+        stack.append(get_turtle_state(turtle))
+        for j in range(bho):
+                for i in range(iterations):
+                        turtle.penup()
+                        restore_turtle_state(turtle, stack.popleft())
+                        turtle.pendown()
+                        for s in range(0,sides):
+                                stack.append(get_turtle_state(turtle))
+                                turtle.forward(l)
+                                turtle.left(alpha)
+                l = tresso_formula(l, sides)
+
+
+
+def main_2():
+        sides = 12
+        l = 300
+        stack.append(get_turtle_state(turtle))
+        #draw_polygon(sides, l, stack[0], False)
+        #draw_fractal_polygon(sides, l)
+        #draw_fractal_relative_polygon(3, 100)
+        test_tresso_formula(sides)
 turtle = tu.Turtle()
 stack = deque()
-main()
+main_2()
+input("Press Enter to continue...")
